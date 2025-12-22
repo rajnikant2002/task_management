24295d34a51bf87f6067da7568a8dbc95bca103b
Rajnikant Behera <153164978+rajni2002491@users.noreply.github.com>
2025-12-22 20:38:01 +0530

    first commit

---
+++ lib/home_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  final String title;
+
+  const HomeScreen({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+    );
+  }
+}
